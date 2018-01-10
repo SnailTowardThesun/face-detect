a6cb75c263b5f3b05baf15c0589fae723a73f041
SnailTowaredThesun <hanvskun@hotmail.com>
2018-01-10 13:31:00 +0800

    remove opencv test code

--- trunk/src/main/fd-main.cpp
+++ trunk/src/main/fd-main.cpp
@@ -24,19 +24,6 @@ SOFTWARE.
 
 #include <vector>
 #include "fd-common.hpp"
-#include "opencv2/core.hpp"
-
-static void help() {
-    std::cout
-    << "\n------------------------------------------------------------------\n"
-    << " This program shows the serial out capabilities of cv::Mat\n"
-    << "That is, cv::Mat M(...); cout << M;  Now works.\n"
-    << "Output can be formated to OpenCV, matlab, python, numpy, csv and \n"
-    << "C styles Usage:\n"
-    << "./cvout_sample\n"
-    << "------------------------------------------------------------------\n\n"
-    << std::endl;
-}
 
 int main(int argc, char** argv) {
     int ret = 0;
@@ -44,45 +31,6 @@ int main(int argc, char** argv) {
     std::cout << "face detect version: " << FD_COMMON::VERSION() << std::endl;
     std::cout << "author: " << FD_COMMON::AUTHOR << std::endl;
     std::cout << "email: " << FD_COMMON::EMAIL << std::endl;
-
-    cv::CommandLineParser parser(argc, argv, "{help h||}");
-    if (parser.has("help"))
-    {
-        help();
-        return 0;
-    }
-
-    cv::Mat I = cv::Mat::eye(4, 4, CV_64F);
-    I.at<double>(1,1) = CV_PI;
-    std::cout << "I = \n" << I << ";" << std::endl << std::endl;
-
-    cv::Mat r = cv::Mat(10, 3, CV_8UC3);
-    randu(r, cv::Scalar::all(0), cv::Scalar::all(255));
-
-    std::cout << "r (default) = \n" << r << ";" << std::endl << std::endl;
-    std::cout << "r (matlab) = \n" << format(r, cv::Formatter::FMT_MATLAB) << ";" << std::endl << std::endl;
-    std::cout << "r (python) = \n" << format(r, cv::Formatter::FMT_PYTHON) << ";" << std::endl << std::endl;
-    std::cout << "r (numpy) = \n" << format(r, cv::Formatter::FMT_NUMPY) << ";" << std::endl << std::endl;
-    std::cout << "r (csv) = \n" << format(r, cv::Formatter::FMT_CSV) << ";" << std::endl << std::endl;
-    std::cout << "r (c) = \n" << format(r, cv::Formatter::FMT_C) << ";" << std::endl << std::endl;
-
-    cv::Point2f p(5, 1);
-    std::cout << "p = " << p << ";" << std::endl;
-
-    cv::Point3f p3f(2, 6, 7);
-    std::cout << "p3f = " << p3f << ";" << std::endl;
-
-    std::vector<float> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    std::cout << "shortvec = " << cv::Mat(v) << std::endl;
-
-    std::vector<cv::Point2f> points(20);
-    for (size_t i = 0; i < points.size(); ++i)
-        points[i] = cv::Point2f((float)(i * 5), (float)(i % 7));
-
-    std::cout << "points = " << points << ";" << std::endl;
 
     return ret;
 }
